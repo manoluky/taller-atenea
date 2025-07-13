2ffdceaa03d644c687f503514d19272f60eaa4c4
Manu Carrasco <carrasco.bravo.manuel@gmail.com>
2025-07-12 22:03:19 -0400

    113

--- utils/backendUtils.ts
+++ utils/backendUtils.ts
@@ -4,14 +4,12 @@ export class BackendUtils {
 
   static async crearUsuarioPorAPI(request: APIRequestContext, usuario: any, esNuevo: boolean = true) {
     let email: string;
+
     if (esNuevo) {
       email = (usuario.email.split('@')[0]) + Date.now().toString() + '@' + usuario.email.split('@')[1];
-
     } else {
-      email = usuario.email; // Usar el email existente si no es un nuevo usuario
-
+      email = usuario.email;
     }
-
 
     const response = await request.post('http://localhost:6007/api/auth/signup', {
       headers: {
