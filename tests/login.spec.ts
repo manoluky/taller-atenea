85f28a5f93f0cbefcf3ce5aca12158dd8b2785fb
Manu Carrasco <carrasco.bravo.manuel@gmail.com>
2025-07-12 21:12:43 -0400

    100

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/loginPage';
-import TestData from '../data/TestData.json';
+import TestData from '../data/testData.json';
 import { DashboardPage } from '../pages/dashboardPage';
 import { BackendUtils } from '../utils/backendUtils';
 import fs from 'fs';
